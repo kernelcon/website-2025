1e909d8ea81a2c511b085551411a10078257f544
clevernyyyy <adam.t.schaal@gmail.com>
2025-02-19 11:06:50 -0600

    adding oc consistency and date update

--- src/pages/Agenda/Agenda.js
+++ src/pages/Agenda/Agenda.js
@@ -75,7 +75,7 @@ export default class Agenda extends Component {
         vertical={vert}>
         <TabList>
           {/* <Tab tabFor="schedule">Schedule</Tab> */}
-          <Tab tabFor="keynotes">Keynote</Tab>
+          <Tab tabFor="keynotes">Keynotes</Tab>
           <Tab tabFor="speakers">Speakers</Tab>
           <Tab tabFor="talks">Talks</Tab>
           <Tab tabFor="villages">Villages</Tab>
